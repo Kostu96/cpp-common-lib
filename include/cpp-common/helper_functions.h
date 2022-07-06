f19c57732045b10eec7701558c3313c049d9215c
Konstanty <Kostu96@gmail.com>
2022-07-06 19:46:57 +0200

    Add wide character string conversion

--- include/cpp-common/helper_functions.h
+++ include/cpp-common/helper_functions.h
@@ -6,10 +6,14 @@
 
 #pragma once
 #include <stddef.h>
+#include <string>
 
 namespace ccl {
 
 	bool readFile(const char* filename, char* data, size_t& size, bool binary = false);
 	bool writeFile(const char* filename, const char* data, size_t size, bool binary = false);
+
+	std::string wstringToString(const std::wstring& wideString);
+	std::wstring stringToWstring(const std::string& wideString);
 
 } // namespace ccl
--- source/helper_functions.cpp
+++ source/helper_functions.cpp
@@ -42,4 +42,18 @@ namespace ccl {
         return retVal;
     }
 
+    std::string wstringToString(const std::wstring& wideString)
+    {
+        std::string temp(wideString.length(), ' ');
+        std::copy(wideString.begin(), wideString.end(), temp.begin());
+        return temp;
+    }
+
+    std::wstring stringToWstring(const std::string& string)
+    {
+        std::wstring temp(string.length(), L' ');
+        std::copy(string.begin(), string.end(), temp.begin());
+        return temp;
+    }
+
 } // namespace ccl
